97769e3eab426c4a50b1378eb555d520d661c599
rfaguiar <rfaguiar1@gmail.com>
2016-09-02 17:40:44 -0300

    Modal de estilos.

--- src/main/java/com/brewer/config/WebConfig.java
+++ src/main/java/com/brewer/config/WebConfig.java
@@ -19,6 +19,8 @@ import org.thymeleaf.templateresolver.ITemplateResolver;
 
 import com.brewer.controller.CervejasController;
 
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+
 
 
 @Configuration
@@ -46,6 +48,7 @@ public class WebConfig extends WebMvcConfigurerAdapter implements ApplicationCon
 		SpringTemplateEngine engine = new SpringTemplateEngine();
 		engine.setEnableSpringELCompiler(true);
 		engine.setTemplateResolver(templateResolver());
+		engine.addDialect(new LayoutDialect());
 		return engine;
 	}
 
