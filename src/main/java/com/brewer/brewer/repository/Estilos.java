c19c3382a330d60edc70afbe4e993a69aef1490c
rfaguiar <rfaguiar1@gmail.com>
2016-09-06 18:07:38 -0300

    Conversor de Estilos

---
+++ src/main/java/com/brewer/brewer/repository/Estilos.java
@@ -0,0 +1,11 @@
+package com.brewer.brewer.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.brewer.model.Estilo;
+
+@Repository
+public interface Estilos extends JpaRepository<Estilo, Long>{
+
+}
--- src/main/java/com/brewer/controller/CervejasController.java
+++ src/main/java/com/brewer/controller/CervejasController.java
@@ -8,10 +8,14 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.brewer.brewer.repository.Cervejas;
+import com.brewer.brewer.repository.Estilos;
 import com.brewer.model.Cerveja;
+import com.brewer.model.Origem;
+import com.brewer.model.Sabor;
 
 @Controller
 public class CervejasController {
@@ -19,20 +23,28 @@ public class CervejasController {
 	@Autowired
 	private Cervejas cervejas;
 	
+	@Autowired
+	private Estilos estilos;
+	
 	@RequestMapping("/cervejas/novo")
-	public String novo(Cerveja cerveja) {
-		//Optional<Cerveja> findBySku = cervejas.findBySkuIgnoreCase("AA1111");//Apagar
-		//System.out.println(findBySku.isPresent());
-		
-		return "cerveja/CadastroCerveja";
+	public ModelAndView novo(Cerveja cerveja) {
+		ModelAndView mv = new ModelAndView("cerveja/CadastroCerveja");
+		mv.addObject("sabores", Sabor.values());
+		mv.addObject("estilos", estilos.findAll());
+		mv.addObject("origens", Origem.values());
+		return mv;
 	}
 	
 	@RequestMapping(value = "/cervejas/novo", method = RequestMethod.POST)
-	public String cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes){
-		if(result.hasErrors()){
+	public ModelAndView cadastrar(@Valid Cerveja cerveja, BindingResult result, Model model, RedirectAttributes attributes){
+		/*if(result.hasErrors()){
 			return novo(cerveja);
+		}*/
+		if(cerveja.getEstilo() != null){
+			System.out.println(">>>>>>>>>>>estilo: " +cerveja.getEstilo().getCodigo());
 		}
+		
 		attributes.addFlashAttribute("mensagem", "Cerveja salva com sucesso");
-		return "redirect:/cervejas/novo";
+		return new ModelAndView("redirect:/cervejas/novo");
 	}
 }
