b4acf80ee52ac965b2a7eace0f36ef7fd187647e
Rogerio-PC <Rogerio-PC@192.168.1.4>
2016-09-14 20:57:17 -0300

    Componente parfa Ordenação da tabela

--- src/main/java/com/brewer/thymeleaf/BrewerDialect.java
+++ src/main/java/com/brewer/thymeleaf/BrewerDialect.java
@@ -9,6 +9,7 @@ import org.thymeleaf.standard.StandardDialect;
 
 import com.brewer.thymeleaf.processor.ClassForErrorAttributeTagProcessor;
 import com.brewer.thymeleaf.processor.MessageElementTagProcessor;
+import com.brewer.thymeleaf.processor.OrderElementTagProcessor;
 
 public class BrewerDialect extends AbstractProcessorDialect{
 
@@ -21,6 +22,7 @@ public class BrewerDialect extends AbstractProcessorDialect{
 		final Set<IProcessor> processadores = new HashSet<>();
 		processadores.add(new ClassForErrorAttributeTagProcessor(dialectPrefix));
 		processadores.add(new MessageElementTagProcessor(dialectPrefix));
+		processadores.add(new OrderElementTagProcessor(dialectPrefix));
 		return processadores;
 	}
 
