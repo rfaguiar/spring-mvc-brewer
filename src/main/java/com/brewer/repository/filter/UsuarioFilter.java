6ef0826a3e498479e5726e0d7d85f7c654061a42
Rogerio-PC <Rogerio-PC@192.168.1.2>
2016-09-26 20:32:24 -0300

    Pesquisa de Usuários e Menu Lateral.

---
+++ src/main/java/com/brewer/repository/filter/UsuarioFilter.java
@@ -0,0 +1,36 @@
+package com.brewer.repository.filter;
+
+import java.util.List;
+
+import com.brewer.model.Grupo;
+
+public class UsuarioFilter {
+	
+	private String nome;
+	private String email;
+	private List<Grupo> grupos;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public List<Grupo> getGrupos() {
+		return grupos;
+	}
+
+	public void setGrupos(List<Grupo> grupos) {
+		this.grupos = grupos;
+	}
+}
--- src/main/java/com/brewer/thymeleaf/BrewerDialect.java
+++ src/main/java/com/brewer/thymeleaf/BrewerDialect.java
@@ -8,6 +8,7 @@ import org.thymeleaf.processor.IProcessor;
 import org.thymeleaf.standard.StandardDialect;
 
 import com.brewer.thymeleaf.processor.ClassForErrorAttributeTagProcessor;
+import com.brewer.thymeleaf.processor.MenuAttributeTagProcessor;
 import com.brewer.thymeleaf.processor.MessageElementTagProcessor;
 import com.brewer.thymeleaf.processor.OrderElementTagProcessor;
 import com.brewer.thymeleaf.processor.PaginationElementTagProcessor;
@@ -25,6 +26,7 @@ public class BrewerDialect extends AbstractProcessorDialect{
 		processadores.add(new MessageElementTagProcessor(dialectPrefix));
 		processadores.add(new OrderElementTagProcessor(dialectPrefix));
 		processadores.add(new PaginationElementTagProcessor(dialectPrefix));
+		processadores.add(new MenuAttributeTagProcessor(dialectPrefix));
 		return processadores;
 	}
 
