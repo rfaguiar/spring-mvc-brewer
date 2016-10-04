3fd3c2f78200f656660c1c97c23ab0e9db1e0332
Rogerio-PC <Rogerio-PC@192.168.1.5>
2016-10-03 21:40:16 -0300

    Venda

--- src/main/java/com/brewer/model/Venda.java
+++ src/main/java/com/brewer/model/Venda.java
@@ -8,9 +8,6 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
@@ -20,7 +17,8 @@ import javax.persistence.Table;
 @Table(name = "venda")
 public class Venda extends BaseEntity {
 
-	
+	private static final long serialVersionUID = 1L;
+
 	@Column(name = "data_criacao")
 	private LocalDateTime dataCriacao;
 	
