f4fc105bd44861b20e10dafeb3cba08e1595db31
Rogerio Aguiar <rfaguiar1@gmail.com>
2018-05-08 18:51:31 -0300

    #TK-43 Implementar testes unitarios de Cerveja

--- src/test/java/com/brewer/model/CervejaTest.java
+++ src/test/java/com/brewer/model/CervejaTest.java
@@ -4,6 +4,8 @@ import com.brewer.Constantes;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.math.BigDecimal;
+
 import static org.junit.Assert.*;
 
 public class CervejaTest {
@@ -12,5 +14,32 @@ public class CervejaTest {
     public void testeMetodoGetFotoOuMockQuandoFotoCervejaEstiverNuloOuVazioDeveRetornarImagenFotoMock() {
         Cerveja cerveja = new Cerveja();
         assertEquals(Constantes.IMAGEN_CERVEJA_MOCK, cerveja.getFotoOuMock());
+    }
+
+    @Test
+    public void testeMetodosTransientAPersistencia() {
+        Cerveja cerveja = new Cerveja();
+        cerveja.setCodigo(new Long(1));
+        cerveja.setFoto("fotoTeste");
+        cerveja.setDescricao("descTeste");
+        cerveja.setTeorAlcoolico(new BigDecimal(10));
+        cerveja.setComissao(new BigDecimal(20));
+        cerveja.setQuantidadeEstoque(new Integer(10));
+        cerveja.setContentType("png");
+        cerveja.setUrlFoto("urlFotoTeste");
+        cerveja.setUrlThumbnailFoto("urlThumbNailTeste");
+        cerveja.setNovaFoto(true);
+
+        assertFalse(cerveja.isNova());
+        assertTrue(cerveja.temFoto());
+        assertEquals("fotoTeste", cerveja.getFoto());
+        assertEquals("descTeste", cerveja.getDescricao());
+        assertEquals("10", cerveja.getTeorAlcoolico().toString());
+        assertEquals("20", cerveja.getComissao().toString());
+        assertEquals("10", cerveja.getQuantidadeEstoque().toString());
+        assertEquals("png", cerveja.getContentType());
+        assertEquals("urlFotoTeste", cerveja.getUrlFoto());
+        assertEquals("urlThumbNailTeste", cerveja.getUrlThumbnailFoto());
+        assertTrue(cerveja.isNovaFoto());
     }
 }
