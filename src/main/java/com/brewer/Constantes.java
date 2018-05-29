f8d40176d0678eb6b10bf3f18d5bd8221c7cefaf
Rogerio Aguiar <rfaguiar1@gmail.com>
2018-05-28 21:27:03 -0300

    correção constante estilos

--- src/main/java/com/brewer/Constantes.java
+++ src/main/java/com/brewer/Constantes.java
@@ -11,7 +11,7 @@ public abstract class Constantes {
     public static final String CADASTRO_CERVEJA_VIEW = "cerveja/CadastroCerveja";
     public static final String SABORES = "sabores";
     public static final String ORIGENS = "origens";
-    public static final String ESTILOS = "estilosRepo";
+    public static final String ESTILOS = "estilos";
     public static final String REDIRECT_CERVEJAS_NOVO_VIEW = "redirect:/cervejas/novo";
     public static final String MENSAGEM_VIEW = "mensagem";
     public static final String PAGINADOR_VIEW = "pagina";
